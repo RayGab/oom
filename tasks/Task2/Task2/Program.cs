290c2838fe6fdc566f3307868069dd3ee37776b2
Raygab | Raini <ic17b078@technikum-wien.at>
2018-04-23 16:55:41 +0200

    Task 2
    initial commit

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class computer
+    {
+        private int ID;
+        private string Name;
+        public int GiveName(int iD,string name)
+        {
+            Name = name;
+            return ID;
+        }
+    }
+}
