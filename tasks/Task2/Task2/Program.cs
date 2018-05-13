a98795dcc03602c95c873c85aca32de1e643f6b4
rainhardtgabriel <ic17b078@technikum-wien.at>
2018-05-13 10:32:14 +0200

    added Task 2: + class Mission + constructr: Mission + public Properties : Points(rw), CountActive(ro), Name(ro), State(ro) + public Method : ChangeState + private fields : _name, _points , _isactive , _activationcount -TODO : foreach Mission Writeline -TODO : fetch Missionname out of .pbo file -TODO : change State if points fall lower or equal to zero -TODO : add Missionstates : assigned, unassigned, succeeded, failed

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -3,24 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// using Statements
+using static System.Console;
+using System.ComponentModel;
+/* Task 2
+ * Author : Rainhardt Gabriel
+ * Version : 0-0-0
+ *
+ */
 namespace Task2
 {
+    public class Mission
+    {
+
+        /******************************** Attributes/ Fields *****************************/
+        // the missions name
+        private string _name;
+        // points for achieving missions goals
+        private decimal _points;
+        // current activation state of mission 
+        private Boolean _isactive;
+        /* counter to check if mission has been activated before 
+         * a mission should only be set to active if there is no other mission available
+         * with a counter less than the current selected one
+         */
+        private int activationcount = 0;
+        // probably need to add Placement on Map maybe with 3dVector Struct?
+        // maybe we can also get the missionname out of the file, but I need to figure out how, so not implemented yet
+        /********************************* Properties ************************************/
+        public Boolean State => _isactive;
+        public string Name => _name;
+        public int CountActive => activationcount;
+        public void ChangeState()
+        {
+            activationcount += _isactive ? 1 : 0;
+            _isactive = !_isactive;
+        }
+        public decimal Points {
+            get
+            {
+                if (_points <= 0) throw new Exception("points sind unter Minimum");
+                return _points;
+            }
+            set
+            {
+                _points = value;
+                // check after adding if _points has fallen underneath the floor
+                if (_points <= 0) throw new Exception("points fallen unter Minimum");
+            }
+        }
+        /********************************* Construcor ************************************/
+
+        public Mission(string missionname, decimal points)
+        {
+            //alle Daten überprüfen die eingegeben werden.
+            if (missionname == null || missionname.Length == 0)
+            {
+                throw new Exception("missing missionname");
+            }
+            if (points <= 0)
+            {
+                throw new Exception("missions must contain points at initialisation");
+            }
+            // initialise to default
+            _isactive = false;
+            // initialise with params
+            _name = missionname;
+            // set _points to points
+            Points = points;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-
-    class computer
-    {
-        private int ID;
-        private string Name;
-        public int GiveName(int iD,string name)
-        {
-            Name = name;
-            return ID;
+            try
+            {
+                WriteLine("Creating 1st Object");
+                Mission one = new Mission("Trollolololo", 10);
+                WriteLine("Creating 2nd Object");
+                Mission two = new Mission("Trallalala", 30);
+                WriteLine("Creating 3rd Object");
+                Mission three = new Mission("Mimimimimimi", 20);
+                WriteLine("Print initialised objects");
+                //needs refactoring with foreach loop
+                WriteLine("Missionsname :" + one.Name +     "\t active :" + one.State +   " \t points: " + one.Points + "\tcount of previous activations :" + one.CountActive);
+                WriteLine("Missionsname :" + two.Name +     "\t active :" + two.State +   " \t points: " + two.Points + "\tcount of previous activations :" + two.CountActive);
+                WriteLine("Missionsname :" + three.Name +   "\t active :" + three.State + " \t points: " + three.Points + "\tcount of previous activations :" + three.CountActive);
+                WriteLine("Changing state of two  for the first time. Number of previous activations should be 0");
+                two.ChangeState();
+                WriteLine("Print two");
+                WriteLine("Missionsname :" + two.Name + " \t active :" + two.State + " \t points: " + two.Points + "\tcount of previous activations :" + two.CountActive);
+                WriteLine("Change state again. Number of previous activations should be 1.");
+                two.ChangeState();
+                WriteLine("And add a negative number to points of two");
+                two.Points += -20;
+                WriteLine("missionsname :" + two.Name + " \t active :" + two.State + " \t points: " + two.Points + "\tcount of previous activations :" + two.CountActive);
+                WriteLine("Let points fall to 0 or below");
+                two.Points += -20;
+                //we should not get here
+                WriteLine("missionsname :" + two.Name + " \t active :" + two.State + " \t points: " + two.Points + "\tcount of previous activations :" + two.CountActive);
+            }
+            catch (Exception e)
+            {
+                WriteLine("error :" + e.Message);
+            }
         }
     }
 }
